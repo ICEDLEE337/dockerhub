3bc00819e0855b561424c17a362a98e0640451dd
icedlee337 <lee.e.norris@gmail.com>
2025-02-15 05:40:28 -0600

    chore: add a hash to the image tag

--- tools/scripts/index.ts
+++ tools/scripts/index.ts
@@ -1,12 +1,19 @@
+import { getGitMetrics } from "./get-git-metrics";
 import { parseRepoAndTagFromArgs } from "./parse-repo-and-tag-from-args.function";
 import { DockerConfig, pushToDockerHub } from "./push-to-docker-hub.function";
 
-const { repositoryName, imageTag } = parseRepoAndTagFromArgs();
+const { repositoryName, imageTagPrefix } = parseRepoAndTagFromArgs();
+
+const { clean, hash } = await getGitMetrics();
+
+if(!clean) {
+    throw new Error(`Commit or stash git changes before publishing`)
+}
 
 const config: DockerConfig = {
     username: 'icedlee337',
     repositoryName,
-    imageTag
+    imageTag: `${imageTagPrefix}-${hash}`
 };
 
 pushToDockerHub(config).catch(console.error);
--- tools/scripts/parse-repo-and-tag-from-args.function.ts
+++ tools/scripts/parse-repo-and-tag-from-args.function.ts
@@ -3,6 +3,6 @@ import { resolve } from "path";
 const [executable, program, ...args] = process.argv;
 const [_projectPath] = args;
 const projectPath = _projectPath.trim();
-const [repositoryName, imageTag] = projectPath.split('/').filter(Boolean);
+const [repositoryName, imageTagPrefix] = projectPath.split('/').filter(Boolean);
 
-export const parseRepoAndTagFromArgs = () => ({ repositoryName, imageTag, cwd: resolve(projectPath) });
+export const parseRepoAndTagFromArgs = () => ({ repositoryName, imageTagPrefix, cwd: resolve(projectPath) });
